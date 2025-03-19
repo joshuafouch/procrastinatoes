815fe95d0d1759158628f7b2ada425b2589a96c4
Joshua <joshfouch777@gmail.com>
2025-03-19 12:15:35 -0400

    preload.js for version numbers

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 // This file is the main entry point for the Electron application.
 const { app, BrowserWindow } = require('electron') // import app and browserwindow modules
+const path = require('node:path')
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
   })
 
   win.loadFile('index.html')
