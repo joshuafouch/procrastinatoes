6099fea4df89f663434e7649589b89e42f376d55
Joshua <joshfouch777@gmail.com>
2025-03-22 15:47:43 -0400

    finished basic functionality

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const taskInput = document.getElementById('task-input');
     const taskList = document.getElementById('tasks-list');
     const addBtn = document.getElementById('add-task');
+    const doneSound = new Audio('./assets/goodjob.mp3');
 
     // event listener when something is inputted
     addBtn.addEventListener('click', function() {
@@ -13,8 +14,8 @@ document.addEventListener('DOMContentLoaded', () => {
         const text = document.createTextNode(taskInput.value);  // text inputted stored in var
 
         const delBtn = document.createElement('button');    // a new button element <button>
-        delBtn.textContent = 'Delete';   // will say 'Delete'
-        delBtn.classList.add('del-btn');    // add a class to the button so we can style it
+        delBtn.textContent = 'Done';   // will say 'Done'
+        delBtn.classList.add('done-btn');    // add a class to the button so we can style it
 
         if (taskInput.value !== '') {
             li.appendChild(text);
@@ -25,4 +26,21 @@ document.addEventListener('DOMContentLoaded', () => {
         }
 
     });
+
+    // event listener for deleting.
+    taskList.addEventListener('click', function(event) {
+        if (event.target.classList.contains('done-btn')) {
+            const listItem = event.target.parentElement;    // this is the <li> the user clicks
+            
+            if (listItem.style.textDecoration === 'line-through') {
+                listItem.style.textDecoration = 'none';
+                event.target.textContent = 'Done';
+            } else {
+                doneSound.currentTime = 0;
+                doneSound.play();
+                listItem.style.textDecoration = 'line-through';
+                event.target.textContent = 'Undo';
+            }
+        }
+    })
 });
